112420a56724936a57aa57503b57aeeca49931b4
luizgrqueiroz <luizgrqueiroz@gmail.com>
2022-11-20 16:54:09 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,17 @@
-function App() {
+import NotificationButton from "./components/NotificationButton";
+
+function App () {
   return (
-    <h1>Olá mundo</h1>
+    <>
+    <h1>a mlk toma diff se liga nos botao</h1>
+    <NotificationButton/>
+    <NotificationButton/>
+    <NotificationButton/>
+    <NotificationButton/>
+    <NotificationButton/>
+    <NotificationButton/>
+    </>
   )
 }
 
-export default App
+export default App;
